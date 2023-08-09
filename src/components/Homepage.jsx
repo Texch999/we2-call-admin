d7abc4aed8c6e3c193aa4febd592305f9aad8a01
s7tech <srikanths7tec@gmail.com>
2023-08-09 18:59:07 +0530

    added home

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -5,12 +5,14 @@ import React from "react";
 import { BsArrowRight } from "react-icons/bs";
 import ManagementContainer from "../pages/home/ManagementContainer";
 import UpcomingAndSummary from "../pages/home/UpcomingAndSummary";
+import TopWinners from "../pages/home/TopWinners";
 
 function Homepage() {
   return (
     <div className="homepage">
       <ManagementContainer />
       <UpcomingAndSummary />
+      <TopWinners />
     </div>
   );
 }
