a4a4347a9a07f2e239293b960b65a06a067953a9
s7tech <srikanths7tec@gmail.com>
2023-08-17 16:00:03 +0530

    added chages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import Header from "./pages/home-page/Header";
 import HomeContent from "./pages/home-page/HomeContent";
 
