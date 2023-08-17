fab4969dcd546ef994c7c02f4e7881446a69e558
Upendarg6 <upendarg6@gmail.com>
2023-08-17 17:07:31 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import Header from "./pages/home-page/Header";
 import HomeContent from "./pages/home-page/HomeContent";
+import CallManagement from "./pages/call-management/CallManagement";
 
 function App() {
   return (
     <div>
       <Header />
-      <HomeContent />
+      <CallManagement />
+      {/* <HomeContent /> */}
     </div>
   );
 }
---
+++ src/pages/call-management/CallManagement.jsx
@@ -0,0 +1,13 @@
+import { Button } from 'bootstrap'
+import React from 'react'
+
+const CallManagement = () => {
+  return (
+    <div>
+      <h1>Call Management</h1>
+   <Button>clikc me</Button>
+    </div>
+  )
+}
+
+export default CallManagement
