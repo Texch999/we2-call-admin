6d0f60db99c095a254603398e3be073fa7816dff
s7tech <srikanths7tec@gmail.com>
2023-08-17 17:08:34 +0530

    added changes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import Header from "./pages/home-page/Header";
 import HomeContent from "./pages/home-page/HomeContent";
+import Marquee from "react-fast-marquee";
 
 function App() {
   return (
     <div>
       <Header />
+      <Marquee className="marqu-tag">
+        Your privacy is our priority. With end-to-end encryption, you can be
+        sure that your personal messages stay between you and who you send them
+        to. Your privacy is our priority. With end-to-end encryption, you can be
+        sure that your personal messages stay between you and who you send them
+        to.
+      </Marquee>
       <HomeContent />
     </div>
   );
