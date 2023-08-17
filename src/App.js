8de778e9eae7fad4ee07a4cd01767fc3a19dd618
s7tech <srikanths7tec@gmail.com>
2023-08-17 15:12:48 +0530

    added changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./pages/home-page/Header";
+import HomeContent from "./pages/home-page/HomeContent";
 
 function App() {
   return (
     <div>
-      <h1>HELLO</h1>
+      <Header />
+      <HomeContent />
     </div>
   );
 }
