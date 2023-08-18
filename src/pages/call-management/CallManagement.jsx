fddf5f76960d4b12475a0e9722458dcec4114275
Upendarg6 <upendarg6@gmail.com>
2023-08-18 12:44:02 +0530

    changes are added

--- src/pages/call-management/CallManagement.jsx
+++ src/pages/call-management/CallManagement.jsx
@@ -5,9 +5,9 @@ import "./style.css";
 import { MdModeEditOutline } from "react-icons/md";
 
 const CallManagement = () => {
-  const callManagementData = [
+  const upcomingMeetingsData = [
     {
-      title: "Sriagent",
+      title: "Ramkrishna - SA",
       event_name:
         "Newzelend vs South Africa Test Series Newzelend Onquard  Stadium",
       date: "19 July 2023, 10:00:00 PM",
@@ -25,7 +25,7 @@ const CallManagement = () => {
       edit: "edit",
     },
     {
-      title: "Sriagent",
+      title: "Ramkrishna - SA",
       event_name:
         "Newzelend vs South Africa Test Series Newzelend Onquard  Stadium",
       date: "19 July 2023, 10:00:00 PM",
@@ -34,21 +34,44 @@ const CallManagement = () => {
       edit: "edit",
     },
   ];
+  const adminNewMeetingsData = [
+    {
+      event_name:
+        "Newzelend vs South Africa Test Series Newzelend Onquard  Stadium",
+      date: "19 July 2023, 10:00:00 PM",
+      admin_status: "Active",
+    },
+    {
+      event_name:
+        "Newzelend vs South Africa Test Series Newzelend Onquard  Stadium",
+      date: "19 July 2023, 10:00:00 PM",
+      admin_status: "In - Active",
+    },
+    {
+      event_name:
+        "Newzelend vs South Africa Test Series Newzelend Onquard  Stadium",
+      date: "19 July 2023, 10:00:00 PM",
+      admin_status: "Active",
+    },
+    {
+      event_name:
+        "Newzelend vs South Africa Test Series Newzelend Onquard  Stadium",
+      date: "19 July 2023, 10:00:00 PM",
+      admin_status: "In - Active",
+    },
+  ];
   return (
     <div className="p-2">
-      <div
-        className="d-flex align-items-center justify-content-between
-    "
-      >
-        <h3 className="mr-4">Call Management</h3>
+      <div className="d-flex align-items-center justify-content-between">
+        <h4>Call Management</h4>
         <Button variant="warning">+ Add New Meetings</Button>
       </div>
       <hr />
       <div>
-        <h3>Upcoming Meetings</h3>
-        <Table className="call-management-data">
+        <h4 className="mb-3">Upcoming Meetings</h4>
+        <Table responsive="md" className="call-management-data">
           <thead>
-            <tr>
+            <tr className="text-center">
               <th>URS</th>
               <th>EVENT NAME</th>
               <th>START DATE & TIME</th>
@@ -58,8 +81,8 @@ const CallManagement = () => {
             </tr>
           </thead>
           <tbody>
-            {callManagementData.map((data, index) => (
-              <tr key={index}>
+            {upcomingMeetingsData?.map((data, index) => (
+              <tr key={index} className="text-center">
                 <td>{data?.title}</td>
                 <td>{data?.event_name}</td>
                 <td>{data?.date}</td>
@@ -75,6 +98,31 @@ const CallManagement = () => {
                     className="bg-secondary text-warning rounded-circle border-0"
                   >
                     <MdModeEditOutline size={18} />
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+      <div>
+        <h4 className="mb-3">Admin New Meetings</h4>
+        <Table responsive="md" className="call-management-data">
+          <thead>
+            <tr>
+              <th scope="col">EVENT NAME</th>
+              <th scope="col">START DATE & TIME</th>
+              <th scope="col">ADMINS</th>
+            </tr>
+          </thead>
+          <tbody>
+            {adminNewMeetingsData?.map((data, index) => (
+              <tr key={index}>
+                <td>{data?.event_name}</td>
+                <td>{data?.date}</td>
+                <td>
+                  <Button className="rounded-pill w-75">
+                    {data?.admin_status}
                   </Button>
                 </td>
               </tr>
