a6353f5c69d8afde1113f60740cb21f7cf72c2c6
Upendarg6 <upendarg6@gmail.com>
2023-08-18 10:34:35 +0530

    changes are added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./pages/home-page/Header";
 import HomeContent from "./pages/home-page/HomeContent";
 import Marquee from "react-fast-marquee";
@@ -9,16 +10,22 @@ import CallManagement from "./pages/call-management/CallManagement";
 function App() {
   return (
     <div>
-      <Header />
-      <Marquee className="marqu-tag">
-        Your privacy is our priority. With end-to-end encryption, you can be
-        sure that your personal messages stay between you and who you send them
-        to. Your privacy is our priority. With end-to-end encryption, you can be
-        sure that your personal messages stay between you and who you send them
-        to.
-      </Marquee>
-      <HomeContent />
-      <CallManagement/>
+              <Header />
+        <Marquee className="marqu-tag">
+          Your privacy is our priority. With end-to-end encryption, you can be
+          sure that your personal messages stay between you and who you send
+          them to. Your privacy is our priority. With end-to-end encryption, you
+          can be sure that your personal messages stay between you and who you
+          send them to.
+        </Marquee>
+      <BrowserRouter>
+
+        <Routes>
+          <Route path="/" element={<HomeContent />} />
+          <Route path="/call-management" element={<CallManagement />} />
+        
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
