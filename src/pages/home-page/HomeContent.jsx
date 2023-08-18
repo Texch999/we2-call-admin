f992a69a347f3fde0949f560ca847edc580895e7
s7tech <srikanths7tec@gmail.com>
2023-08-18 18:31:19 +0530

    added chages

--- src/pages/home-page/HomeContent.jsx
+++ src/pages/home-page/HomeContent.jsx
@@ -16,18 +16,35 @@ function HomeContent() {
             <div className="we2-call-button">we2Call</div>
           </div>
           <div className="w-50 d-flex align-items-center justify-content-center">
-            <img className="call-management-image" src={Images.headphone_with_microphone}></img>
+            <img
+              className="call-management-image"
+              src={Images.headphone_with_microphone}
+            ></img>
           </div>
         </div>
         <div className="col-5 col-sm-12 col-md-12 col-lg-5 col-xl-5 management">
-          <div className="user-management  d-flex p-3">
-            <div >
-              <img className="management-image" src={Images.SportManagement}></img>
+          <div className="user-management  d-flex p-3 align-items-center">
+            <div>
+              <img
+                className="management-image"
+                src={Images.SportManagement}
+              ></img>
+            </div>
+            <div>
+              <h2>Sports Management</h2>
+              <h5>Create your new meetings</h5>
             </div>
           </div>
-          <div className="sports-management h-50 p-3">
+          <div className="sports-management  p-3 d-flex align-items-center">
             <div>
-            <img className="management-image" src={Images.UserManagement}></img>
+              <img
+                className="management-image"
+                src={Images.UserManagement}
+              ></img>
+            </div>
+            <div>
+              <h2>User Management</h2>
+              <h5>Create your new meetings</h5>
             </div>
           </div>
         </div>
