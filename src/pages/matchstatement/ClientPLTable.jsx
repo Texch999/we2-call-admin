666efc5d81271d8232cdfdfa8b94fe4225bc8320
sangram96rath <skesharirath96@gmail.com>
2023-08-29 17:51:37 +0530

    changes

--- src/pages/matchstatement/ClientPLTable.jsx
+++ src/pages/matchstatement/ClientPLTable.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import ClientPLData from "./ClientPLData";
+import AdminsTable from "../onepagereport/AdminsTable";
+import { Col, Container, Row } from "react-bootstrap";
 
 function ClientPLTable() {
   const CLIENTPL_DETAILS = [
@@ -51,13 +53,85 @@ function ClientPLTable() {
       fancycom: "500000.00",
       mfc: "500000.00",
     },
+    {
+      name: "Animesh",
+      matchpl: "1000000.00",
+      sixover: "500000.00",
+      tenover: "500000.00",
+      fifteenover: "500000.00",
+      sixoverone: "500000.00",
+      tenoverone: "500000.00",
+      fifteenoverone: "500000.00",
+      fancycom: "500000.00",
+      mfc: "500000.00",
+    },
+    {
+      name: "Animesh",
+      matchpl: "1000000.00",
+      sixover: "500000.00",
+      tenover: "500000.00",
+      fifteenover: "500000.00",
+      sixoverone: "500000.00",
+      tenoverone: "500000.00",
+      fifteenoverone: "500000.00",
+      fancycom: "500000.00",
+      mfc: "500000.00",
+    },
+    {
+      name: "Animesh",
+      matchpl: "1000000.00",
+      sixover: "500000.00",
+      tenover: "500000.00",
+      fifteenover: "500000.00",
+      sixoverone: "500000.00",
+      tenoverone: "500000.00",
+      fifteenoverone: "500000.00",
+      fancycom: "500000.00",
+      mfc: "500000.00",
+    },
+    {
+      name: "Animesh",
+      matchpl: "1000000.00",
+      sixover: "500000.00",
+      tenover: "500000.00",
+      fifteenover: "500000.00",
+      sixoverone: "500000.00",
+      tenoverone: "500000.00",
+      fifteenoverone: "500000.00",
+      fancycom: "500000.00",
+      mfc: "500000.00",
+    },
+    {
+      name: "Animesh",
+      matchpl: "1000000.00",
+      sixover: "500000.00",
+      tenover: "500000.00",
+      fifteenover: "500000.00",
+      sixoverone: "500000.00",
+      tenoverone: "500000.00",
+      fifteenoverone: "500000.00",
+      fancycom: "500000.00",
+      mfc: "500000.00",
+    },
+    {
+      name: "Animesh",
+      matchpl: "1000000.00",
+      sixover: "500000.00",
+      tenover: "500000.00",
+      fifteenover: "500000.00",
+      sixoverone: "500000.00",
+      tenoverone: "500000.00",
+      fifteenoverone: "500000.00",
+      fancycom: "500000.00",
+      mfc: "500000.00",
+    },
   ];
   const [showClientPL, setShowClientPL] = useState(false);
   const handleClientData = () => {
-    setShowClientPL((prev)=>!prev);
+    setShowClientPL((prev) => !prev);
   };
   return (
-    <div>
+    <div className="d-flex flex-row">
       <table className="w-100 match-position-table small-font">
         <thead>
           <tr className="text-center">
@@ -76,7 +150,7 @@ function ClientPLTable() {
         {CLIENTPL_DETAILS.map((item, index) => (
           <tbody key={index}>
             <tr className="text-center">
-              <td onClick={() => handleClientData()}>{item.name}</td>
+              <td>{item.name}</td>
               <td className="clr-green">{item.matchpl}</td>
               <td className="clr-green">{item.sixover}</td>
               <td className="clr-green"> {item.tenover}</td>
@@ -85,7 +159,16 @@ function ClientPLTable() {
               <td className="clr-green"> {item.tenoverone}</td>
               <td className="clr-green"> {item.fifteenoverone}</td>
               <td className="clr-green"> {item.fancycom}</td>
-              <td className="clr-green"> {item.mfc}</td>
+              <td
+                className="clr-green d-flex flex-column align-items-center"
+                onClick={() => handleClientData()}
+              >
+                {item.mfc}
+                <img
+                  className="click-here-img mt-1"
+                  src={process.env.PUBLIC_URL + "./assets/click_here.png"}
+                ></img>
+              </td>
             </tr>
           </tbody>
         ))}
