76329a9f11844cdca632c4711ac827557f7e79af
vinod120 <vinodgate2016@gmail.com>
2023-09-19 15:26:30 +0530

    changes

--- src/pages/home-page/AlertsTable.jsx
+++ src/pages/home-page/AlertsTable.jsx
@@ -1,8 +1,27 @@
-import React from "react";
 import moment from "moment";
 
 function AlertsTable(props) {
   const { data, columns, tableClassname } = props;
+  if (!data?.length) {
+    return (
+      <>
+        <table
+          className={`w-100 match-position-table text-center medium-font ${tableClassname}`}
+        >
+          <thead id="home-table-head">
+            <tr>
+              {columns.map((column, index) => (
+                <th key={index}>{column.header}</th>
+              ))}
+            </tr>
+          </thead>
+        </table>
+        <div className="no-data-found">
+          <p>No Data Found</p>
+        </div>
+      </>
+    );
+  }
   return (
     <table
       className={`w-100 match-position-table text-center medium-font ${tableClassname}`}
@@ -15,50 +34,53 @@ function AlertsTable(props) {
         </tr>
       </thead>
       <tbody>
-        {data.map((item, rowIndex) => (
-          <tr key={rowIndex}>
-            {columns.map((column, colIndex) => (
-              <td key={colIndex}>
-                <div
-                  className={
-                    item[column.field] === "Edited"
-                      ? "td-class"
-                      : item[column.field] === "Not-Started"
-                      ? "not-class"
-                      : ""
-                  }
-                >
-                  {colIndex === 1 ? (
-                    <div>
+        {data?.length > 0 &&
+          data.map((item, rowIndex) => (
+            <tr key={rowIndex}>
+              {columns.map((column, colIndex) => (
+                <td key={colIndex}>
+                  <div
+                    className={
+                      item[column.field] === "Edited"
+                        ? "td-class"
+                        : item[column.field] === "Not-Started"
+                        ? "not-class"
+                        : ""
+                    }
+                  >
+                    {colIndex === 1 ? (
                       <div>
-                        {item.team}-
-                        <span
-                          className={
-                            item.pe === "P" ? "clr-green me-2" : "clr-red me-2"
-                          }
-                        >
-                          {item.pe}
-                        </span>
-                        {Object.keys(item?.teamObj)[0]} vs
-                        {Object.keys(item?.teamObj)[1]}
+                        <div>
+                          {item.team}-
+                          <span
+                            className={
+                              item.pe === "P"
+                                ? "clr-green me-2"
+                                : "clr-red me-2"
+                            }
+                          >
+                            {item.pe}
+                          </span>
+                          {Object.keys(item?.teamObj)[0]} vs
+                          {Object.keys(item?.teamObj)[1]}
+                        </div>
+                        <div>
+                          {item.date}
+                          <span className="ms-2">
+                            {moment(item?.matchTimeStamp).format(
+                              "DD-MM-YYYY, hh:mm:ss"
+                            )}
+                          </span>
+                        </div>
                       </div>
-                      <div>
-                        {item.date}
-                        <span className="ms-2">
-                          {moment(item?.matchTimeStamp).format(
-                            "DD-MM-YYYY, hh:mm:ss"
-                          )}
-                        </span>
-                      </div>
-                    </div>
-                  ) : (
-                    item[column.field]
-                  )}
-                </div>
-              </td>
-            ))}
-          </tr>
-        ))}
+                    ) : (
+                      item[column.field]
+                    )}
+                  </div>
+                </td>
+              ))}
+            </tr>
+          ))}
       </tbody>
     </table>
   );
