76187f2a208f483917762e7f9e38e9270963f26e
RanjithKatari <katariranjith@gmail.com>
2023-12-29 14:54:47 +0530

    Changes

--- src/components/video-call/Calling.jsx
+++ src/components/video-call/Calling.jsx
@@ -1,19 +1,30 @@
+import { useState } from "react";
 import ScoreBoard from "./ScoreBoard";
 import UserList from "./UserList";
 import "./styles.css";
 
 function Calling() {
+  const [activeIndex, setActiveIndex] = useState(1);
   const buttons = ["Join Users", "Score Board"];
+  const handleButton = (index) => {
+    setActiveIndex(index);
+  };
   return (
     <div className="w-25 header-bg h-80vh rounded p-1">
       <div className="w-100 num-btn-bg rounded p-2">
         <div className="d-flex align-items-center justify-content-between font-14">
-          <button className="rounded-pill py-1 px-3 border-none bg-yellow">
-            Join Users
-          </button>
-          <button className="rounded-pill py-1 px-3 border-none bg-blue">
-            Score Board
-          </button>
+          {buttons?.length > 0 &&
+            buttons?.map((data, index) => (
+              <button
+                key={index}
+                className={`rounded-pill py-1 px-3 border-none ${
+                  activeIndex === index ? "bg-yellow" : "bg-blue"
+                }`}
+                onClick={() => handleButton(index)}
+              >
+                {data}
+              </button>
+            ))}
         </div>
         <div className="d-flex align-items-center justify-content-between font-14 mt-2">
           <div className="rounded-pill py-1 px-3 bg-blue">28-12-2024</div>
@@ -21,8 +32,8 @@ function Calling() {
           <div className="rounded-pill py-1 px-3 bg-blue">IND vs AUS</div>
         </div>
       </div>
-      <UserList />
-      <ScoreBoard />
+      {activeIndex === 0 && <UserList />}
+      {activeIndex === 1 && <ScoreBoard />}
     </div>
   );
 }
